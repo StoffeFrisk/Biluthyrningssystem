1845d084f517607ad0b3968b517045cb6a3fea1d
tiefr <tie.frisk@hotmail.com>
2025-04-23 15:53:13 +0200

    Making CarService and CarRepository

---
+++ src/main/java/com/example/Biluthyrningssystem/services/CarService.java
@@ -0,0 +1,18 @@
+package com.example.Biluthyrningssystem.services;
+
+import com.example.Biluthyrningssystem.entities.Car;
+
+import java.util.List;
+
+public interface CarService {
+
+    public List<Car> getAllCars();
+
+    public Car getCarById(Long id);
+
+    public Car addCar(Car car);
+
+    public Car updateCar(String id, Car updateCar);
+
+    public void deleteCar(Long id);
+}
