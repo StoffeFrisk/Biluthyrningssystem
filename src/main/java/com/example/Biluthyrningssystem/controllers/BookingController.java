539d3cd97348bb8a0c1fe33229a7d573a355418d
BP <valkarian@valkarian.com>
2025-04-23 09:23:34 +0200

    Task: Created empty Booking classes - service, repository, entity, controller

---
+++ src/main/java/com/example/Biluthyrningssystem/controllers/BookingController.java
@@ -0,0 +1,4 @@
+package com.example.Biluthyrningssystem.controllers;
+
+public class BookingController {
+}
---
+++ src/main/java/com/example/Biluthyrningssystem/services/BookingServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.Biluthyrningssystem.services;
+
+public class BookingServiceImpl implements BookingService{
+}
