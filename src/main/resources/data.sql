b3d477ee1b68db02d128603b1e29c72313868e7b
JavaLynz <lynseyfoxstudent@gmail.com>
2025-04-24 11:37:06 +0200

    added ResourceNotFoundException, completed methods in customerService, set up customer endpoints

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 //Lynsey Fox
 
 INSERT INTO customer (personnummer, first_name, last_name, address_id, email, phone) VALUES
-('19850101-1234','Anna','Svensson',,,),
-('19900215-5678','Erik','Johansson',,,),
-('19751230-9101','Maria','Lindberg',,,),
-('19881122-3456','Johan','Karlsson',,,),
-(' 19950505-7890','Elin','Andersson',,,);
+('19850101-1234','Anna','Svensson',1,,),
+('19900215-5678','Erik','Johansson',2,,),
+('19751230-9101','Maria','Lindberg',3,,),
+('19881122-3456','Johan','Karlsson',4,,),
+(' 19950505-7890','Elin','Andersson',5,,);
 
 INSERT INTO address (street, postal_code, city) VALUES
 ('Jönköpingsvägen', '55129', 'Huskvarna'),
@@ -13,4 +13,5 @@ INSERT INTO address (street, postal_code, city) VALUES
 ('123 Maple Street', '12345', 'Springfield'),
 ('456 Oak Avenue', '67890', 'Shelbyville'),
 ('789 Pine Road', '54321', 'Ogdenville');
+
 
